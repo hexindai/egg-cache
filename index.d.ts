6f0014eb3d7ce7f216a15945397d44c6c58ebd40
jasine <jasinechen@gmail.com>
2019-04-12 14:46:17 +0800

    fix: 完善函数重载

--- index.d.ts
+++ index.d.ts
@@ -1,32 +1,39 @@
-interface LruOpts {
-  max: number;
-  maxAge: number;
-  dispose: string;
-  length: number;
-  stale: string;
+interface SetValueFunc {
+  (): any;
+  (): Promise<any>;
+}
+
+interface SetFunc {
+  (key: string, value: SetValueFunc): Promise<void>;
+  (key: string, value: any, expire?: number, options?: any): Promise<void>;
+}
+
+interface GetValueFunc {
+  (): any;
+}
+
+interface GetFunc {
+  (key: string): Promise<any>;
+  (key: string, defaultValue: any): Promise<any>;
+  (key: string, GetValueFunc, expire?: number, options?: any): Promise<any>;
 }
 
 interface Store {
-  set: (key: string, value: any, expire?: number, options?: object) => void;
-  get: (key: string) => any;
-  del: (key: string) => void;
-  has: (key: string) => boolean;
+  set: (key: string, value: any) => Promise<any>;
+  get: (key: string) => Promise<void>;
+  del: (key: string) => Promise<void>;
+  has: (key: string) => Promise<boolean>;
 }
 
 declare module "egg" {
   export interface Application {
     cache: {
-      set: (
-        key: string,
-        value: any,
-        expire?: number,
-        options?: LruOpts
-      ) => void;
-      get: (key: string) => any;
-      del: (key: string) => void;
-      has: (key: string) => boolean;
+      set: SetFunc;
+      get: GetFunc;
+      del: (key: string) => Promise<void>;
+      has: (key: string) => Promise<boolean>;
       store: (name: string, options?: object) => Store;
-      reset: () => void;
+      reset: () => Promise<void>;
     };
   }
 }
